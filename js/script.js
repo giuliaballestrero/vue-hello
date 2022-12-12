db3c5f2a2dbca5406ff56d0d8fc553a1c8982238
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-12-12 15:38:57 +0100

    Add image using Vue

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ const { createApp } = Vue;
 createApp ( {
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue',
+            title: 'Coding Cat',
+            text: 'Look at this coding cat, it looks purr-fect!',
+            image: 'https://images.unsplash.com/photo-1634838083208-ce7a36701fe4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
         }
     }
 }).mount('#app');
